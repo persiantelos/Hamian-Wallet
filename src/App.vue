5758f475c3311573b324206891c065575d770bd4
mohammad goudarzi <webmaster.webdeveloper@gmail.com>
2021-10-08 11:03:21 -0700

    add bg-grey-10

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="q-app"> 
+  <div id="q-app" class="bg-grey-10"> 
     <router-view />
   </div>
 </template>
@@ -7,7 +7,20 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';  
 import BaseLocalService from 'src/localService/baseLocalService'
 import StorageService from 'src/localService/storageService' 
+import CommonService from 'src/services/commonService';
 
+Vue.mixin({
+    preFetch:(option:any)=>{
+      async function getNetworks(){
+        var network = await CommonService.getNetworks()
+        if(network){
+          // console.log('====> n etworks',network)
+          return option.store.dispatch('getBlockChains',network);
+        }
+      }
+  getNetworks();
+  }
+})
 @Component({ 
   components:{
   }
